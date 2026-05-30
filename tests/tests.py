95f4e73c0a82d0cad93e95821a8594901eac2aa0
enlace-one <contact_enlace@proton.me>
2026-05-30 15:05:44 -0500

    Improved test cases

--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from pathlib import Path
 
@@ -8,7 +9,7 @@ SRC_DIR = ROOT_DIR / "src"
 sys.path.insert(0, str(SRC_DIR))
 
 from typer.testing import CliRunner
-from socx.socx2 import app, find, unwrap_url
+from socx import socx
 
 runner = CliRunner()
 
@@ -17,6 +18,9 @@ TEST_FILES_DIR = ROOT_DIR / "tests" / "test_files"
 print(f"Using test files in {TEST_FILES_DIR}")
 
 last_result = None
+
+# Command To Test:
+# python "src/socx/socx.py"
 
 
 # ----------------#
@@ -31,7 +35,7 @@ def run_cli(args):
         args = args.split()
 
     last_result = runner.invoke(
-        app,
+        socx.app,
         args,
         catch_exceptions=False,
     )
@@ -44,60 +48,6 @@ def run_cli(args):
 # ----------------#
 
 
-def test_find_command():
-
-    result = run_cli(
-        [
-            "find",
-            "PhineasAndFerb.txt",
-            "--directory",
-            str(TEST_FILES_DIR),
-        ]
-    )
-
-    print(f"Result output: {result.output}")
-
-    assert result.exit_code == 0
-    # try:
-    assert "PhineasAndFerb.txt" in result.output
-    # except:
-    #     print(f"File name not in {result.stdout}. {result.stderr}")
-    #     assert "PhineasAndFerb.txt" in result.stdout
-
-
-def test_find_command_not_found():
-
-    result = run_cli(
-        [
-            "find",
-            "--filename",
-            "NotARealFile.txt",
-            "--directory",
-            str(TEST_FILES_DIR),
-        ]
-    )
-
-    assert result.exit_code == 0
-    assert "not found" in result.stdout.lower()
-
-
-def test_regex_find():
-
-    result = run_cli(
-        [
-            "find",
-            "--filename",
-            "Phineas.*",
-            "--directory",
-            str(TEST_FILES_DIR),
-            "--regex",
-        ]
-    )
-
-    assert result.exit_code == 0
-    assert "PhineasAndFerb.txt" in result.stdout
-
-
 def test_unwrap_url():
 
     wrapped_url = (
@@ -105,7 +55,7 @@ def test_unwrap_url():
         "?url=https%3A%2F%2Fgoogle.com"
     )
 
-    result = unwrap_url(wrapped_url)
+    result = socx.unwrap_url(wrapped_url)
 
     assert "google.com" in result
 
@@ -126,6 +76,31 @@ def test_default_banner():
     assert "Version:" in result.stdout
 
 
+def test_combine_csvs():
+
+    result = run_cli(
+        [
+            "combine",
+            "--directory",
+            str(TEST_FILES_DIR),
+        ]
+    )
+
+    assert result.exit_code == 0
+    assert "\\tests\\test_files\\socx_combined_file" in result.stdout.lower()
+    saved_file_path = result.stdout.lower().split("saved")[1].strip()
+    expected_content = """name, id,source
+bob,1,assets.csv
+ted,2,assets2.csv"""
+    with open(saved_file_path, "r") as f:
+        content = f.read().strip()
+        if content != expected_content:
+            print(content)
+        assert content == expected_content
+
+    os.remove(saved_file_path)
+
+
 def test_combine_no_csvs():
 
     empty_dir = TEST_FILES_DIR / "empty"
@@ -142,6 +117,88 @@ def test_combine_no_csvs():
 
     assert result.exit_code == 0
     assert "not enough csvs" in result.stdout.lower()
+
+
+def test_determine_argument_type_ip():
+    ip = "8.8.8.8"
+    result = socx.determine_info_argument_type(ip)
+    if result != "ip":
+        print(f"Result was {result}")
+    assert result == "ip"
+
+
+def test_determine_argument_type_domain():
+    domain = "google.com"
+    result = socx.determine_info_argument_type(domain)
+    if result != "domain":
+        print(f"Result was {result}")
+    assert result == "domain"
+
+
+def test_determine_argument_type_url():
+    url = "https://google.com"
+    result = socx.determine_info_argument_type(url)
+    if result != "url":
+        print(f"Result was {result}")
+    assert result == "url"
+
+
+def test_info_command_ip():
+    ip = "8.8.8.8"
+    result = run_cli(["info", ip])
+    assert "dns.google" in result.stdout
+    assert "Pingable: True" in result.stdout
+    assert "Organization: Google" in result.stdout
+
+
+def test_find_command():
+
+    result = run_cli(
+        [
+            "find",
+            "PhineasAndFerb.txt",
+            "--directory",
+            str(TEST_FILES_DIR),
+        ]
+    )
+
+    assert result.exit_code == 0
+    # try:
+    assert "PhineasAndFerb.txt" in result.output
+    # except:
+    #     print(f"File name not in {result.stdout}. {result.stderr}")
+    #     assert "PhineasAndFerb.txt" in result.stdout
+
+
+def test_find_command_not_found():
+
+    result = run_cli(
+        [
+            "find",
+            "NotARealFile.txt",
+            "--directory",
+            str(TEST_FILES_DIR),
+        ]
+    )
+
+    assert result.exit_code == 0
+    assert "not found" in result.stdout.lower()
+
+
+def test_regex_find():
+
+    result = run_cli(
+        [
+            "find",
+            "Phineas.*",
+            "--directory",
+            str(TEST_FILES_DIR),
+            "--regex",
+        ]
+    )
+
+    assert result.exit_code == 0
+    assert "PhineasAndFerb.txt" in result.stdout
 
 
 # ----------------#
